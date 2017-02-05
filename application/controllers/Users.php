af9a9a0559583e6cf47927ba6e3eca0e994d720b
Namwoo Kim <iam@kimtree.net>
2017-02-06 02:47:32 +0900

    Modify Users route to serve /user/me URI correctly

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -2,9 +2,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Users extends CI_Controller {
-	public function index()
+	public function detail($user_id)
 	{
-		echo 'mypage';
+		echo $user_id . ' mypage';
 	}
 
 	public function me()
