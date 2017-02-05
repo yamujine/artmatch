8ece4a90c0975fabfbe9bb443340f50b43436ae1
Namwoo Kim <iam@kimtree.net>
2017-02-06 02:28:15 +0900

    Unified models into upper model and add a new model

--- application/models/Artwork_images_model.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Artwork_images_model extends CI_Model
-{
-	/**
-	 * table_name: artwork_images
-	 */
-}
--- application/models/Place_images_model.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Place_images_model extends CI_Model
-{
-	/**
-	 * table_name: place_images
-	 */
-}
