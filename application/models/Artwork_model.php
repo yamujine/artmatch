260e5840e9724ee557b64cc00bb88d72491f00dc
Namwoo Kim <iam@kimtree.net>
2017-02-09 04:05:31 +0900

    Add insert function into artwork model

--- application/models/Artwork_model.php
+++ application/models/Artwork_model.php
@@ -5,4 +5,45 @@ class Artwork_model extends CI_Model
 	/**
 	 * table_name: artworks
 	 */
+	const TABLE_NAME = 'artworks';
+
+	//public $id; -- Ignore PK
+	public $user_id;
+	public $status;
+	public $title;
+	public $description;
+	public $image;
+	public $for_sale;
+	public $use_comment;
+	public $tags;
+
+	public function insert($user_id, $status, $title, $description, $image, $for_sale, $use_comment, $tags)
+	{
+		$this->_fill_class_variable_with_params($user_id, $status, $title, $description, $image, $for_sale, $use_comment, $tags);
+		if ($this->db->insert(self::TABLE_NAME, $this)) {
+			return $this->db->insert_id();
+		} else {
+			return NULL;
+		}
+	}
+
+	/**
+	public function update($id, $user_id, $status, $title, $description, $image, $for_sale, $use_comment, $tags)
+	{
+		$this->_fillClassVariableWithParams($user_id, $status, $title, $description, $image, $for_sale, $use_comment, $tags);
+		return $this->db->update(self::TABLE_NAME, $this, ['id' => $id]);
+	}
+	 **/
+
+	private function _fill_class_variable_with_params($user_id, $status, $title, $description, $image, $for_sale, $use_comment, $tags)
+	{
+		$this->user_id = $user_id;
+		$this->status = $status;
+		$this->title = $title;
+		$this->description = $description;
+		$this->image = $image;
+		$this->for_sale = $for_sale;
+		$this->use_comment = $use_comment;
+		$this->tags = $tags;
+	}
 }
