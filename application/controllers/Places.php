a3e26d7bb8f746af29d5b2078ac98198c5269ffb
june.kim <junho85@gmail.com>
2017-02-08 22:01:08 +0900

    route list

--- application/controllers/Places.php
+++ application/controllers/Places.php
@@ -1,7 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Place extends CI_Controller {
+class Places extends CI_Controller {
+    public function index() {
+        echo "장소 목록 입니다.";
+    }
+
 	public function detail($place_number)
 	{
 		echo $place_number . ' place detail';
