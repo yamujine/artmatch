a2e0a59584fb7d0d87bf60f08984d9ec0346c271
geine <bonos2@wisdo.me>
2017-02-10 01:30:23 +0900

    Check empty session about user

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,18 +1,23 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Login extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
-        $this->load->library('session');
     }
+
     public function index()
     {
+        $this->load->library('session');
         $user = array(
             'email' => $this->session->userdata('email'),
             'auth' => $this->session->userdata('is_auth')
-            );
+        );
+        if (empty($user)) {
+            $user = NULL;
+        }
 
         $this->load->view('login', $user);
     }
