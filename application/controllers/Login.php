c5ffe25410158f1f0763fb45bd8c5435e8f6f0b5
geine <bonos2@wisdo.me>
2017-02-11 04:30:48 +0900

    Fix check session logic

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,22 +1,23 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Login extends CI_Controller
+class Login extends MY_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
     public function index()
     {
         $this->load->library('session');
-        $user = array(
-            'email' => $this->session->userdata('email'),
-            'auth' => $this->session->userdata('is_auth')
-        );
-        if (empty($user)) {
+
+        $email = $this->session->userdata('email');
+        $auth = $this->session->userdata('is_auth');
+
+        if (empty($email) && empty($auth)) {
             $user = NULL;
+            echo 'empty session';
+        } else {
+            $user = array(
+                'email' => $email,
+                'auth' => $auth
+            );
         }
 
         $this->load->view('login', $user);
