b90dbc089fafcc76a53efc987c677f53bdc95c22
Namwoo Kim <iam@kimtree.net>
2017-02-10 18:38:29 +0900

    Add a imageupload library

--- application/controllers/Artworks.php
+++ application/controllers/Artworks.php
@@ -11,7 +11,7 @@ class Artworks extends MY_Controller
 	public function upload()
 	{
 		$this->load->model('artwork_model');
-		$this->load->library(['form_validation', 'upload', 'tag']);
+		$this->load->library(['form_validation', 'upload', 'tag', 'imageupload']);
 		$this->load->helper('url');
 
 		// Form validation
@@ -31,7 +31,7 @@ class Artworks extends MY_Controller
 				$user_id = 9282;
 
 				// Upload representative image first
-				$uploaded_image_name = $this->_upload_images('image');
+				$uploaded_image_name = $this->imageupload->upload_images('image');
 				$result_id = $this->artwork_model->insert(
 					$user_id,
 					$this->input->post('status'),
@@ -44,9 +44,10 @@ class Artworks extends MY_Controller
 				);
 				if ($result_id !== NULL) {
 					// Upload extra images
-					$uploaded_image_names = $this->_upload_bulk_images('extra_images');
-					$this->artwork_model->insert_images($result_id, $uploaded_image_names);
-
+					$uploaded_image_names = $this->imageupload->upload_bulk_images('extra_images');
+					if (!empty($uploaded_image_names)) {
+						$this->artwork_model->insert_images($result_id, $uploaded_image_names);
+					}
 					redirect('/artworks/' . $result_id);
 				} else {
 					$data['error'] = 'Failed to insert into DB';
@@ -60,71 +61,5 @@ class Artworks extends MY_Controller
 		}
 
 		$this->twig->display('artworks/upload', $data);
-	}
-
-	private function _upload_images($param, $generate_thumbs = true)
-	{
-		// Load upload library
-		$this->upload->initialize([
-			'upload_path' => './uploads/',
-			'allowed_types' => 'gif|jpg|png|jpeg',
-			'file_ext_tolower' => TRUE,
-			'max_size' => 2048, // 2MB
-			'encrypt_name' => TRUE
-		]);
-
-		// Upload Image
-		$this->upload->do_upload($param);
-
-		// Generate Thumbnails
-		if ($generate_thumbs) {
-			$this->_generate_thumbnails($this->upload->data('full_path'));
-		}
-
-		return $this->upload->data('file_name');
-	}
-
-	private function _upload_bulk_images($param, $generate_thumbs = true)
-	{
-		$this->upload->initialize([
-			'upload_path' => './uploads/',
-			'allowed_types' => 'gif|jpg|png|jpeg',
-			'file_ext_tolower' => TRUE,
-			'max_size' => 2048, // 2MB
-			'encrypt_name' => TRUE
-		]);
-
-		// Upload Images
-		$this->upload->do_multi_upload($param);
-
-		foreach ($this->upload->get_multi_upload_data() as $uploaded_data) {
-			if ($generate_thumbs) {
-				$this->_generate_thumbnails($uploaded_data['full_path']);
-			}
-			$uploaded_file_names[] = $uploaded_data['file_name'];
-		}
-
-		return $uploaded_file_names;
-	}
-
-	private function _generate_thumbnails($original_image_path)
-	{
-		/*
-		 * _thumb -> 512x512
-		 * _thumb_small -> 128x128
-		 */
-		$this->load->library('image_lib');
-		$default_config = [
-			'image_library' => 'gd2',
-			'source_image' => $original_image_path,
-			'create_thumb' => TRUE,
-			'maintain_ratio' => TRUE
-		];
-
-		$this->image_lib->initialize(array_merge($default_config, ['width' => 512, 'height' => 512]));
-		$this->image_lib->resize();
-
-		$this->image_lib->initialize(array_merge($default_config, ['width' => 128, 'height' => 128, 'thumb_marker' => '_thumb_small']));
-		$this->image_lib->resize();
 	}
 }
