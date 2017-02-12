f2b76b4772c248eb59e58154c6ad704a6ca336bc
Namwoo Kim <iam@kimtree.net>
2017-02-13 03:06:45 +0900

    Add a user signup validation and refactor twig

--- application/controllers/UsersApi.php
+++ application/controllers/UsersApi.php
@@ -4,32 +4,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class UsersApi extends API_Controller {
     public function __construct() {
         parent::__construct();
-        $this->load->library(['email', 'encryption', 'twig']);
+        $this->load->library(['email', 'encryption', 'form_validation', 'twig']);
         $this->load->helper('url');
         $this->load->model('user_model');
     }
 
     public function register() {
-        /**
-         * @todo Need validation check
-         */
-        $hash = password_hash($this->input->post('password'), PASSWORD_BCRYPT);
-        $id = $this->user_model->add(
-            $this->input->post('email'),
-            $hash,
-            $this->input->post('user_name'),
-            $this->input->post('profile_image'),
-            $this->input->post('type')
-        );
+    	// Validation 관련 설정
+        $this->_configure_form_validations();
 
-        if ($id) {
-            $user = $this->user_model->get_by_id($id);
-            $this->_set_user_session($user);
-            $this->_send_mail($user->email, $user->id);
-            $this->set_success_response(['message' => 'signup success']);
-        } else {
-            $this->set_fail_response('101', ['message' => 'duplicated email']);
-        }
+		if ($this->form_validation->run() === TRUE) {
+			$hashed_password = password_hash($this->input->post('password'), PASSWORD_BCRYPT);
+			$id = $this->user_model->add(
+				$this->input->post('email'),
+				$hashed_password,
+				$this->input->post('user_name'),
+				$this->input->post('profile_image'),
+				$this->input->post('type')
+			);
+
+			if ($id) {
+				$user = $this->user_model->get_by_id($id);
+				$this->_set_user_session($user);
+				$this->_send_mail($user->email, $user->id);
+				$this->set_success_response(['message' => 'signup success']);
+			} else {
+				$this->set_fail_response('101', ['message' => 'duplicated email']);
+			}
+		} else {
+			$this->set_fail_response('105', ['message' => $this->form_validation->error_string()]);
+		}
 
         return $this->output->set_output(json_encode($this->result));
     }
@@ -75,7 +79,29 @@ class UsersApi extends API_Controller {
         return $this->output->set_output(json_encode($this->result));
     }
 
-    private function _send_mail($user_email, $user_id) {
+	private function _configure_form_validations() {
+		// Register validation
+		$this->form_validation->set_error_delimiters('', "\r\n");
+		$this->form_validation->set_rules('email', '이메일', 'required|valid_email|is_unique[users.email]', [
+			'required' => '이메일 주소가 입력되지 않았습니다.',
+			'valid_email' => '유효한 이메일 주소가 아닙니다.',
+			'is_unique' => '중복된 이메일 주소입니다.'
+		]);
+		$this->form_validation->set_rules('password', '패스워드', 'required', [
+			'required' => '패스워드가 입력되지 않았습니다.'
+		]);
+		$this->form_validation->set_rules('user_name', '유저 아이디', 'required|alpha_dash|is_unique[users.user_name]', [
+			'required' => '유저 아이디가 입력되지 않았습니다.',
+			'alpha_dash' => '아이디에 사용할 수 없는 문자열이 포함되어 있습니다. (영 소문자, 숫자, -, _ 만 가능)',
+			'is_unique' => '중복된 유저 아이디입니다.'
+		]);
+		$this->form_validation->set_rules('type', '회원 구분', 'required|in_list[0,1]', [
+			'required' => '회원 구분이 입력되지 않았습니다.',
+			'in_list' => '회원 구분값이 올바르지 않습니다.'
+		]);
+	}
+
+	private function _send_mail($user_email, $user_id) {
 		$data['code'] = urlencode($this->encryption->encrypt($user_email . '/' . $user_id));
 		$email_html = $this->twig->render('email/verify', $data);
 
