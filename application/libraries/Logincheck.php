8b10340c049ff06fa7c5c0c9ac02a1f7fd3cc2cf
geine <bonos2@wisdo.me>
2017-02-12 18:26:09 +0900

    Add get_user

--- application/libraries/Logincheck.php
+++ application/libraries/Logincheck.php
@@ -31,7 +31,7 @@ class Logincheck {
 
     public function get_user_id() {
         if ($this->_validate_session()) {
-            return $this->CI->session->userdata('user_id');
+            return $this->CI->session->userdata('id');
         }
         return FALSE;
     }
@@ -43,17 +43,32 @@ class Logincheck {
         return FALSE;
     }
 
+    public function get_user() {
+        if ($this->_validate_session()) {
+            $user = new stdClass();
+            $user->id = $this->CI->session->userdata('id');
+            $user->email = $this->CI->session->userdata('email');
+            $user->type = $this->CI->session->userdata('type');
+            $user->user_name = $this->CI->session->userdata('user_name');
+            $user->profile_image = $this->CI->session->userdata('profile_image');
+            $user->is_auth = $this->CI->session->userdata('is_auth');
+            $user->is_admin = $this->CI->session->userdata('is_admin');
+            return $user;
+        }
+        return FALSE;
+    }
+
     private function _validate_session() {
 
         $id = $this->CI->session->userdata('id');
         $email = $this->CI->session->userdata('email');
         $type = $this->CI->session->userdata('type');
-        $user_id = $this->CI->session->userdata('user_name');
+        $user_name = $this->CI->session->userdata('user_name');
         $profile_image = $this->CI->session->userdata('profile_image');
         $is_auth = $this->CI->session->userdata('is_auth');
         $is_admin = $this->CI->session->userdata('is_admin');
 
-        if ($id == '' || $email == '' || $type == '' || $user_id == '' || $profile_image == '' || $is_auth == '' || $is_admin == '') {
+        if ($id == '' || $email == '' || $type == '' || $user_name == '' || $profile_image == '' || $is_auth == '' || $is_admin == '') {
             return FALSE;
         }
         return TRUE;
