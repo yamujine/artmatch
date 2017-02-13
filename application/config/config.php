701c286f265da067360704be7cf3cc5d6c1a724a
june.kim <junho85@gmail.com>
2017-02-13 23:35:49 +0900

    dynamic base_url #76

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://www.pickartyou.com/';
+$protocol = "http";
+if (!empty($_SERVER['HTTPS'])) {
+    $protocol = "https";
+}
+$config['base_url'] = $protocol.'://'.$_SERVER['HTTP_HOST'].':'.$_SERVER['SERVER_PORT'].'/';
+
 /*
 |--------------------------------------------------------------------------
 | Index File
