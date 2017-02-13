e4d5309fdf1c15959394dd632d75ff003e099df2
Namwoo Kim <iam@kimtree.net>
2017-02-13 20:30:33 +0900

    Add a landing page

---
+++ application/controllers/Landing.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// MY_Controller가 로그인 체크를 하여 /account/login route로 redirect를 하므로, 이 컨트롤러는 MY_Controller을 사용하지 않음
+class Landing extends CI_Controller {
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('twig');
+    }
+
+    public function index() {
+        $this->twig->display('landing/landing');
+    }
+}
