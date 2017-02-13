9b36ab9c3dc7405c0acf6bbf58aa23d9959d91a8
Namwoo Kim <iam@kimtree.net>
2017-02-13 20:31:44 +0900

    Fix simple indentation

--- application/models/Landing_model.php
+++ application/models/Landing_model.php
@@ -20,6 +20,7 @@ class Landing_model extends CI_Model {
     public function insert($email) {
         $this->email = $email;
         $this->registered_at = date("Y-m-d H:i:s");
+
         return $this->db->insert(self::TABLE_NAME, $this);
     }
 }
