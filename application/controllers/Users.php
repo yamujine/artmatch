13a98aff835e7fde7a0d65bcb5542a3b926790d0
nablyy <selll9402@gmail.com>
2017-02-20 03:54:20 +0900

    Fix a bug

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,49 +1,39 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Users extends MY_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->model(['user_model', 'pick_model', 'artwork_model', 'place_model']);
     }
-
     public function detail($user_name) {
         $data = $this->_get_user_details($user_name);
-
         $this->twig->display('users/mypage', $data);
     }
-
     public function me() {
         $user_name = $this->accountlib->get_user_name();
         $pick_type = $this->input->get('type');
-
         $data = $this->_get_user_details($user_name, true, $pick_type);
-
         $this->twig->display('users/mypage', $data);
     }
-
     private function _get_user_details($user_name, $is_my_page = false, $pick_type = '') {
         $user = $this->user_model->get_by_user_name($user_name);
-
         // 내 작품, 장소 리스트
-        if ((int) $user->type === 0) {
+        if ((int)$user->type === 0) {
             $mine = $this->artwork_model->get_by_user_id($user->id);
-        } else if ((int) $user->type === 1) {
+        } else if ((int)$user->type === 1) {
             $mine = $this->place_model->get_by_user_id($user->id);
         }
         foreach ($mine as $something) {
-            $type = ((int) $user->type === 0) ? 'artworks' : 'places';
+            $type = ((int)$user->type === 0) ? 'artworks' : 'places';
             $something->url = '/' . $type . '/' . $something->id;
             $something->subject = ($type === 'artworks') ? $something->title : $something->name;
         }
-
         // 받은 pick 카운트
-        if ((int) $user->type === 0) {
+        if ((int)$user->type === 0) {
             $given_pick_count = $this->pick_model->get_given_artwork_pick_by_user_id($user->id);
-        } else if ((int) $user->type === 1) {
+        } else if ((int)$user->type === 1) {
             $given_pick_count = $this->pick_model->get_given_place_pick_by_user_id($user->id);
         }
-
         // 내가 pick한 작품 장소 리스트
         $my_picks = [];
         if ($is_my_page && !empty($pick_type)) {
@@ -62,7 +52,6 @@ class Users extends MY_Controller {
                 $my_pick->subject = $my_pick->place->name;
             }
         }
-
         return [
             'user' => $user,
             'is_my_page' => $is_my_page,
