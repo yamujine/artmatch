8f2a863d428b61de2cf68b66037f465c0165f9e3
geine <bonos2@wisdo.me>
2017-02-20 15:19:26 +0900

    Modify code indent

--- application/libraries/Imageupload.php
+++ application/libraries/Imageupload.php
@@ -99,8 +99,8 @@ class Imageupload {
 
     public function upload_image_by_url($url, $generate_thumbs = true, $image_type = '') {
         $file_ext = pathinfo(basename(parse_url($url)['path']), PATHINFO_EXTENSION);
-        $filename = $this->CI->security->sanitize_filename(md5(uniqid(mt_rand()))) .'.'.$file_ext;
-        $image_path = self::UPLOAD_PATH.$image_type.'/'.$filename;
+        $filename = $this->CI->security->sanitize_filename(md5(uniqid(mt_rand()))) . '.' . $file_ext;
+        $image_path = self::UPLOAD_PATH . $image_type . '/' . $filename;
         $result = file_put_contents($image_path, file_get_contents($url));
         if ($result !== FALSE) {
             if ($generate_thumbs) {
