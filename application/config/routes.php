cb2f49a59cf52fd47cb983040becddc4419b0803
Namwoo Kim <iam@kimtree.net>
2017-02-22 15:40:50 +0900

    Change routes with case-sensitive class names

--- application/config/routes.php
+++ application/config/routes.php
@@ -65,21 +65,21 @@ $route['places/edit/(:num)'] = 'places/edit/$1';
 $route['users/me'] = 'users/me'; // Don't change ordering
 $route['users/(:any)'] = 'users/detail/$1'; // Don't change ordering
 
-$route['api/comments'] = 'commentapi';
-$route['api/comments/delete'] = 'commentapi/delete';
-$route['api/comments/insert'] = 'commentapi/insert';
-$route['api/comments/update'] = 'commentapi/update';
+$route['api/comments'] = 'commentApi';
+$route['api/comments/delete'] = 'commentApi/delete';
+$route['api/comments/insert'] = 'commentApi/insert';
+$route['api/comments/update'] = 'commentApi/update';
 
 $route['api/contents/(:any)'] = 'main/api/$1';
 
-$route['api/pick']['POST'] = 'pickapi';
+$route['api/pick']['POST'] = 'pickApi';
 
-$route['api/login']['POST'] = 'usersapi/login';
-$route['api/users/register']['POST'] = 'usersapi/register';
-$route['api/users/verify'] = 'usersapi/verify';
-$route['api/users/check_username'] = 'usersapi/check_username';
-$route['api/users/check_email'] = 'usersapi/check_email';
-$route['api/users/update_image']['POST'] = 'usersapi/update_profile_image';
-$route['api/users/change_password']['POST'] = 'usersapi/change_password';
-$route['api/users/reset_password']['POST'] = 'usersapi/reset_password';
-$route['api/landing'] = 'landingapi';
+$route['api/login']['POST'] = 'usersApi/login';
+$route['api/users/register']['POST'] = 'usersApi/register';
+$route['api/users/verify'] = 'usersApi/verify';
+$route['api/users/check_username'] = 'usersApi/check_username';
+$route['api/users/check_email'] = 'usersApi/check_email';
+$route['api/users/update_image']['POST'] = 'usersApi/update_profile_image';
+$route['api/users/change_password']['POST'] = 'usersApi/change_password';
+$route['api/users/reset_password']['POST'] = 'usersApi/reset_password';
+$route['api/landing'] = 'landingApi';
