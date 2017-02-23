79e0eb33853066f7a9c3a2d7cc2a602bdb759838
geine <bonos2@wisdo.me>
2017-02-24 04:43:13 +0900

    Add Facebook env

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['app_id'] = '';
-$config['app_secret'] = '';
+$config['app_id'] = $_ENV['CI_PAY_FB_APP_ID'];
+$config['app_secret'] = $_ENV['CI_PAY_FB_SECRET_KEY'];
+$config['api_version'] = 'v2.8';
