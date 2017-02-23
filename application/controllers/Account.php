c20690ef3a923fe2ff0a792ec1ebcf663ec8809c
Namwoo Kim <iam@kimtree.net>
2017-02-23 09:57:53 +0900

    Add a thumb_url functions in account controller

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -11,6 +11,33 @@ class Account extends CI_Controller {
         /** @var Twig_Environment $twig */
         $twig = $this->twig->getTwig();
         /**
+         * 썸네일 이미지 URL 리턴하는 함수
+         */
+        $twig->addFilter(new Twig_SimpleFilter('thumb_url', function ($filename, $type = '', $size = '_thumb_small') {
+            if (empty($filename)) {
+                return ''; // Fallback error image
+            }
+
+            if (ENVIRONMENT === 'production') {
+                $host = 'http://img.pickartyou.com/';
+            } else {
+                $host = '/uploads/';
+            }
+
+            if (!empty($type)) {
+                $host .= $type . '/';
+            }
+
+            $path_parts = pathinfo($filename);
+
+            if (!isset($path_parts['extension'])) {
+                return ''; // Fallback error image
+            }
+
+            return $host . $path_parts['filename'] . $size . '.' . $path_parts['extension'];
+        }));
+
+        /**
          * static URL 리턴
          */
         $twig->addFilter(new Twig_SimpleFilter('static_url', function ($filename) {
