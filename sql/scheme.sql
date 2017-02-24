b3e241bebf3e968ea4ff19a5d4a4f440b32f27b5
Namwoo Kim <iam@kimtree.net>
2017-02-25 05:09:05 +0900

    Fix scheme

--- sql/scheme.sql
+++ sql/scheme.sql
@@ -238,10 +238,10 @@ DROP TABLE IF EXISTS `pickartyou_dev`.`apply` ;
 
 CREATE TABLE IF NOT EXISTS `pickartyou_dev`.`apply` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `place_id` INT NOT NULL,
-  `artwork_id` INT NOT NULL,
-  `status` TINYINT NOT NULL,
-  `registered_at` DATETIME NOT NULL,
+  `place_id` INT NOT NULL COMMENT '장소 PK',
+  `artwork_id` INT NOT NULL COMMENT '작품 PK',
+  `status` TINYINT NOT NULL COMMENT '지원 상태',
+  `registered_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT '지원 시간',
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
