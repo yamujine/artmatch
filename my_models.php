0321be18630be5516a0af2084ccc2838b2600390
june.kim <junho85@gmail.com>
2017-02-25 14:56:59 +0900

    작품/장소 삭제 분리된 comment model 사용

--- my_models.php
+++ my_models.php
@@ -18,11 +18,13 @@
  * @property film_model       $film_model
  * @property film_model1      $film_model1
  *
- * @property Artwork_model          $artwork_model              artwork comment model
- * @property Place_model            $place_model                place comment model
+ * @property Artwork_model          $artwork_model              artwork model
+ * @property Place_model            $place_model                place model
  * @property Comment_model          $comment_model              deprecated comment model
  * @property Artwork_Comment_model  $artwork_comment_model      artwork comment model
  * @property Place_Comment_model    $place_comment_model        place comment model
+ * @property Exhibition_model       $exhibition_model           exhibition model
+ * @property Pick_model             $pick_model                 pick model
  *
  */
 class my_models
