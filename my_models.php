7408e6ed6a0bb856680d860640328584c6a42836
june.kim <junho85@gmail.com>
2017-02-25 13:29:30 +0900

    PhpStorm 에서 CodeIgniter model 자동 완성 #246

---
+++ my_models.php
@@ -0,0 +1,30 @@
+<?php die();
+
+/**
+ * Add you custom models here that you are loading in your controllers
+ *
+ * <code>
+ * $this->site_model->get_records()
+ * </code>
+ * Where site_model is the model Class
+ *
+ * ---------------------- Models to Load ----------------------
+ * <examples>
+ *
+ * @property membership_model $membership_model
+ * @property site_model       $site_model
+ * @property data_model       $data_model
+ * @property products_model   $products_model
+ * @property film_model       $film_model
+ * @property film_model1      $film_model1
+ *
+ * @property Comment_model          $comment_model              deprecated comment model
+ * @property Artwork_Comment_model  $artwork_comment_model      deprecated comment model
+ * @property Place_Comment_model    $place_comment_model        deprecated comment model
+ *
+ */
+class my_models
+{
+}
+
+// End my_models.php
