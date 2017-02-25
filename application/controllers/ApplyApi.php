2b86962fde83e2ee750056609098462d4a9b4bcc
geine <bonos2@wisdo.me>
2017-02-25 12:44:49 +0900

    Add apply api

---
+++ application/controllers/ApplyApi.php
@@ -0,0 +1,78 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ApplyApi extends API_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model(['apply_model','exhibition_model', 'place_model']);
+    }
+
+    public function accept_applied_artworks() {
+        $applied_artwork_ids = $this->input->post('applied_artwork_ids');
+        $exhibition_id = $this->input->post('exhibition_id');
+        $msg = NULL;
+
+        if (!$this->_is_my_exhibition($exhibition_id)) {
+            $msg = '본인의 전시만 수락 할 수 있습니다.';
+        }
+
+        if (!$this->_is_applied_artworks($exhibition_id, $applied_artwork_ids)) {
+            $msg = '본인의 전시에 지원한 작품만 수락할 수 있습니다.';
+        }
+
+        if ($msg !== NULL) {
+            $this->return_fail_response('501', ['message' => $msg]);
+        }
+
+        foreach ($applied_artwork_ids as $applied_artwork_id) {
+            $result = $this->apply_model->update_status($exhibition_id, $applied_artwork_id);
+            $id = $this->exhibition_model->insert_exhibition_artworks($exhibition_id, $applied_artwork_id);
+
+            if ($id === false || $result === false) {
+                $this->return_fail_response('500', ['message' => '데이터베이스 업데이트 에러']);
+            }
+        }
+        $this->return_success_response();
+    }
+
+    /**
+     * 본인의 전시인지 체크
+     * @param $exhibition_id
+     * @return bool
+     */
+    private function _is_my_exhibition($exhibition_id) {
+        $exhibition = $this->exhibition_model->get_exhibition_by_id($exhibition_id);
+
+        if ($exhibition === NULL) {
+            return false;
+        }
+
+        $place = $this->place_model->get_by_id($exhibition->place_id, $this->accountlib->get_user_id());
+
+        if ($place === NULL) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * 해당 전시에 지원한 작품인지 체크
+     * @param $exhibition_id
+     * @param $applied_artworks_ids
+     * @return bool
+     */
+    private function _is_applied_artworks($exhibition_id, $applied_artworks_ids) {
+        foreach ($applied_artworks_ids as $applied_artworks_id) {
+            $result = $this->apply_model->get_by_exhibition_id_and_artwork_id($exhibition_id, $applied_artworks_id);
+
+            if ($result === NULL) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
