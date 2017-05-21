79a6ffdd4530371c7829bc4d9066c98129c3ed44
kimtree <iam@kimtree.net>
2017-05-21 16:58:10 +0900

    Add a monkey patching code for updating exhibition by place id

--- application/models/Exhibition_model.php
+++ application/models/Exhibition_model.php
@@ -55,17 +55,24 @@ class Exhibition_model extends CI_Model {
     }
 
     public function update_by_place_id($place_id, $start_date, $end_date, $artwork_count, $is_free) {
+        // TODO: 현재 함수는 전시 1개를 가정으로 만들어진 함수이므로 나중에 의존성 제거가 필요함
+        // TODO: 아래는 임시로 처음에 장소 등록시 기본으로 생성되는 전시 정보를 가져오는 코드임
+        $default_exhibition = $this->get_one_by_place_id($place_id);
+        if (!$default_exhibition) {
+            return NULL;
+        }
+
         $data = [
             'start_date' => $start_date,
             'end_date' => $end_date,
             'artwork_count' => $artwork_count,
             'is_free' => $is_free
         ];
-        if ($this->db->update(self::TABLE_NAME, $data, ['place_id' => $place_id])) {
-            return $place_id;
-        } else {
-            return NULL;
+        if ($this->db->update(self::TABLE_NAME, $data, ['id' => $default_exhibition->id])) {
+            return $default_exhibition->id;
         }
+
+        return NULL;
     }
 
     public function get_one_by_place_id($place_id) {
